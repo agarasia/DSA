38f1ba99f379da9bba52a6dffa88cd45d2dcf28d
agarasia <agarasia@asu.edu>
2024-05-13 18:34:01 -0700

    fixed spiral matrix

--- Arrays/Medium/Spiral Matrix.py
+++ Arrays/Medium/Spiral Matrix.py
@@ -1,27 +1,28 @@
-def spiralMatrix(matrix):
-    row = len(matrix)
-    col = len(matrix[0])
-    top, left = 0, 0
-    bottom, right = col-1, row-1
+def spiralMatrix(A):
+    row, col = len(A), len(A[0])
+    top, left = 0, 0 
+    bottom, right = row - 1, col - 1
     ans = []
-
-    while top <= bottom and left <= right:
+    
+    while top<=bottom and left<=right:
         for i in range(left, right+1):
-            ans.append(matrix[top][i])
+            ans.append(A[top][i])
         top += 1
-
+        
         for i in range(top, bottom+1):
-            ans.append(matrix[i][right])
+            ans.append(A[i][right])
         right -= 1
-
-        for i in range(right, left-1, -1):
-            ans.append(matrix[bottom][i])
-        bottom -= 1
-
-        for i in range(bottom, top-1, -1):
-            ans.append(matrix[i][left])
-        left += 1
-
+        
+        if top <= bottom:
+            for i in range(right, left-1, -1):
+                ans.append(A[bottom][i])
+            bottom -= 1
+        
+        if left <= right:
+            for i in range(bottom, top-1, -1):
+                ans.append(A[i][left])
+            left += 1
+            
     return ans
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
