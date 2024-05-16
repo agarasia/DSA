6172bd6c4594d7341f1dd292c49dfaa321bae263
agarasia <agarasia@asu.edu>
2024-05-16 09:42:03 -0700

    Finished Subarray Sum Equals K optimal

--- Arrays/Medium/Subarray Sum Equals K.py
+++ Arrays/Medium/Subarray Sum Equals K.py
@@ -1,3 +1,4 @@
+# Approach : Brute Force (Find all possible contiguous subarrays)
 def subarraySumBruteForce(nums, target):
     count = 0
 
@@ -10,6 +11,24 @@ def subarraySumBruteForce(nums, target):
 
     return count
 
+# Approach : Use a hash map <- optimal
+def subarraySumOptimal(nums, target):
+    count = 0
+    h_map = {0 : 1}     # Adding the key 1 to '0' as there will be
+                        # an empty array case.
+    Sum = 0
+    for i in range(len(nums)):
+        Sum += nums[i]      # Keeping track of the prefix sum
+
+        if Sum - target in h_map:   # if remaining sum is in map already,
+            count += h_map[Sum - target]
+        if Sum in h_map:            # if the prefix sum exists in map
+            h_map[Sum] += 1
+        else:
+            h_map[Sum] = 1
+    
+    return count
+
 nums = [1,1,1]
 k = 2
-print("Count : ", subarraySumBruteForce(nums, k))
+print("Count : ", subarraySumBruteForce(nums, k), subarraySumOptimal(nums, k))
