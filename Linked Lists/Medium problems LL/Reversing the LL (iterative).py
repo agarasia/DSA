f39e488e8d0030da807132e68ce7b0ce34b05e05
agarasia <agarasia@asu.edu>
2024-06-06 14:22:20 -0700

    Reversing LL iteratively

---
+++ Linked Lists/Medium problems LL/Reversing the LL (iterative).py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class LL:
+    def __init__(self, head = None):
+        self.head = head
+    
+    def append(self, newNode:Node):
+        if not self.head:
+            self.head = newNode
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            
+            curr.next = newNode
+
+    def print(self):
+        curr = self.head
+        while curr:
+            print(curr.data, end=" -> ")
+            curr = curr.next
+        print()
+
+    def reverse(self):
+        curr = self.head
+        prev = None
+
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        
+        self.head = prev
+        
+
+
+ll = LL()
+ll.append(Node(1))
+ll.append(Node(2))
+ll.append(Node(3))
+ll.append(Node(4))
+ll.print()
+ll.reverse()
+ll.print()
