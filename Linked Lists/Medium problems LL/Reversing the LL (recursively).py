9e07c49d668e1f3ccc3602737b97e4467217281a
agarasia <agarasia@asu.edu>
2024-06-06 14:30:15 -0700

    Reversing the LL recursively

---
+++ Linked Lists/Medium problems LL/Reversing the LL (recursively).py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class LL:
+    def __init__(self, head = None):
+        self.head = head
+    
+    def append(self, newNode:Node):
+        if not self.head:
+            self.head = newNode
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            
+            curr.next = newNode
+
+    def print(self):
+        curr = self.head
+        while curr:
+            print(curr.data, end=" -> ")
+            curr = curr.next
+        print()
+
+
+# Approach: Divide the problem into smaller subproblems
+#           and solve the base problem.
+def reverse(head):
+    if not head or not head.next:
+        return head
+    
+    prev = None
+    newHead = reverse(head.next)
+    head.next.next = head
+    head.next = prev
+
+    return newHead
+# T(n) = O(n)   
+
+
+ll = LL()
+ll.append(Node(1))
+ll.append(Node(2))
+ll.append(Node(3))
+ll.append(Node(4))
+ll.print()
+ll.head = reverse(ll.head)
+ll.print()
