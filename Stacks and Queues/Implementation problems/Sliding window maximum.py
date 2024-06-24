dc7bff74812489b4aa7dd32ba462a4c92434901a
agarasia <agarasia@asu.edu>
2024-06-24 14:14:16 -0700

    Sliding window maximum Optimal Approach

--- Stacks and Queues/Implementation problems/Sliding window maximum.py
+++ Stacks and Queues/Implementation problems/Sliding window maximum.py
@@ -1,7 +1,10 @@
 # Approach: Do what we are asked to do! Create a
 #           sliding window of size k and append its
 #           maximum element to the ans array.
-def slidingWindowMaximum(nums, k):
+from collections import deque
+
+
+def slidingWindowMaximumBruteForce(nums, k):
     def getMax(ind1, ind2):
         Max = float('-inf')
         for i in range(ind1, ind2):
@@ -17,6 +20,30 @@ def slidingWindowMaximum(nums, k):
 #                   each element twice to get the maximum.
 # S(n)= O(k)        storing the answer.
 
+# Approach: Employ a queue to keep track of which number comes in
+#           and which number goes out.
+def slidingWindowMaximumOptimal(nums, k):
+    res = [] 
+    left, right = 0, 0
+    q = deque()
+
+    while right < len(nums):
+        while q and nums[right] > nums[q[-1]]:
+            q.pop()
+        q.append(right)
+
+        if left > q[0]:
+            q.popleft()
+        
+        if right + 1 >= k:
+            res.append(nums[q[0]])
+            left += 1
+        right += 1
+    
+    return res
+# T(n) = O(n)   where n = length of nums
+# S(n) = O(n)   for storing queue and res
+
 nums = [4, 0, -1, 3, 5, 3, 6 ,8]
 
-print(slidingWindowMaximum(nums, 3))
+print(slidingWindowMaximumBruteForce(nums, 3))
