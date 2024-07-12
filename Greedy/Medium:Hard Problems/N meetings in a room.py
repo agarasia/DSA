7af0ff3a7c1951f6e68e922192fdde40107979b8
agarasia <agarasia@asu.edu>
2024-07-12 14:57:33 -0700

    N Meetings in a room

---
+++ Greedy/Medium:Hard Problems/N meetings in a room.py
@@ -0,0 +1,27 @@
+# Approach: Sort the meetings in order of their ending
+#           so that a room is free at the very next meeting.
+def numberOfMeetings(start, end):
+    meetings = []
+    for i in range(n):
+        meetings.append([start[i], end[i]])
+    
+    sorted_meetings = sorted(meetings, key=lambda x: x[1])
+
+    # Initialize the list of selected meetings with the first meeting
+    selected_meetings = [sorted_meetings[0]]
+    last_end_time = sorted_meetings[0][1]
+    
+    # Iterate through the remaining meetings
+    for i in range(1, len(sorted_meetings)):
+        if sorted_meetings[i][0] > last_end_time:
+            selected_meetings.append(sorted_meetings[i])
+            last_end_time = sorted_meetings[i][1]
+    
+    return len(selected_meetings)
+# T(n) = O(n * logn)
+# S(n) = O(n)
+
+start = [1,3,0,5,8,5]
+end = [2,4,6,7,9,9]
+
+print(numberOfMeetings(start, end))
