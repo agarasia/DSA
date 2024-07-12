cbc7420b22b59f0850dfd15c29a29e49932bddf6
agarasia <agarasia@asu.edu>
2024-07-12 16:12:14 -0700

    Jump Game Followup

---
+++ Greedy/Medium:Hard Problems/Jump Game 2.py
@@ -0,0 +1,17 @@
+def numberOfJumps(nums):
+    res = 0
+    l, r = 0, 0
+
+    while r < len(nums)-1:
+        farthest = 0
+        for i in range(l, r+1):
+            farthest = max(farthest, i + nums[i])
+        l = r + 1
+        r = farthest
+        res += 1
+    
+    return res
+
+nums = [2, 3, 1, 1, 4]
+
+print(numberOfJumps(nums))
