fc6eeb8141f4c69142ebcefd400a407ab3492f2b
agarasia <agarasia@asu.edu>
2024-07-11 21:54:45 -0700

    Coin Change Problem

---
+++ Greedy/Easy problems/Coin Change Problem.py
@@ -0,0 +1,18 @@
+# Approach: Our aim is to find the greedy way,
+#           NOT the correct number of coins needed.
+def numOfCoins(V, coins):
+    num = 0
+    n = len(coins)
+    for i in range(n-1, -1, -1):
+        while V >= coins[i]:
+            V -= coins[i]
+            num += 1
+    
+    return num
+# T(n) = O(V)
+# S(n) = O(1)
+
+V = 49
+coins = [1, 2, 5, 10, 20, 50, 100, 500, 1000]
+
+print(numOfCoins(V, coins))
