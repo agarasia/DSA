b318c22d47e434fc1c0432d0a7849452faae9808
agarasia <agarasia@asu.edu>
2024-08-08 19:27:00 -0700

    Surrounded Regions

---
+++ Graphs/Problems on BFS or DFS/Surrounded Regions.py
@@ -0,0 +1,39 @@
+def surroundedRegions(grid):
+    R, C = len(grid), len(grid[0])
+
+    def capture(r, c):
+        if (r < 0 or r == R
+            or c < 0 or c == C or
+            grid[r][c] != 1):
+            return
+        
+        grid[r][c] = -1
+        capture(r + 1, c)
+        capture(r - 1, c)
+        capture(r, c - 1)
+        capture(r, c + 1)
+    
+    # Getting all the unsurrounded regions
+    for i in range(R):
+        for j in range(C):
+            if (grid[i][j] == 1 and 
+                (i in [0, R - 1] or j in [0, C - 1])):
+                capture(i, j)
+
+    # Capturing the surrounded regions
+    for i in range(R):
+        for j in range(C):
+            if grid[i][j] == -1:
+                grid[i][j] = 1
+            elif grid[i][j] == 1:
+                grid[i][j] = 0
+# T(n) = O(n * m)
+# S(n) = O(1)
+
+grid = [[0, 0, 0, 0],
+        [1, 0, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]]
+print(grid)
+surroundedRegions(grid)
+print(grid)
